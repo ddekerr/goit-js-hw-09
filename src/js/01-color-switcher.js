7f8895541f3efc66260f388d1bb4278c67ccef3b
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-07-05 15:14:15 +0300

    First task completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+let idInterval = null;
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', startChangeBodyColor);
+btnStop.addEventListener('click', stopChangeBodyColor);
+
+// start changing body color every second
+function startChangeBodyColor() {
+  idInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor()
+  }, 1000);
+  toogleDisabledButton();
+}
+
+// stop changing body color
+function stopChangeBodyColor() {
+  clearInterval(idInterval);
+  toogleDisabledButton();
+}
+
+// disabling button if clicked and undisabling another button
+function toogleDisabledButton() {
+  if(btnStart.hasAttribute('disabled')) {
+    btnStart.removeAttribute('disabled');
+    btnStop.setAttribute('disabled', true);
+  } else {
+    btnStart.setAttribute('disabled', true);
+    btnStop.removeAttribute('disabled');
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
