984ea70678f2f038ab7f381be1f97e568837bbcd
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-07-26 22:49:04 +0300

    Bugfix promise function i 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,33 +13,33 @@ refs.createBtn.addEventListener('click', showPromises);
 
 function showPromises(e) {
   e.preventDefault();
+
   // call for loop after tne number of ms that the refs.delay contains
-  setTimeout(() => {
-    for(let i = 1; i <= Number(refs.amount.value); i += 1) {
-      // create i promises and pass them sequence number and delay before calling
-      // use Notify lib to inform Success or Failure
-      createPromise(i, Number(refs.step.value))
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-    }
-  }, Number(refs.delay.value));
+  for(let i = 1; i <= Number(refs.amount.value); i += 1) {
+    // create i promises and pass them sequence number and delay before calling
+    // use Notify lib to inform Success or Failure
+    createPromise(i, Number(refs.delay.value), Number(refs.step.value))
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  }
 }
 
 // creating Promise with random resolve or reject result
-function createPromise(position, delay) {
+function createPromise(position, delay, step) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+    const promiseCalingDelay = position === 1 ? delay : delay + step * (position - 1);
     // call the Promise in delay after calling the previous Promise
     setTimeout(() => {
       if (shouldResolve) {
-        resolve({position, delay: delay * position});
+        resolve({position, delay: promiseCalingDelay});
       } else {
-        reject({position, delay: delay * position});
+        reject({position, delay: promiseCalingDelay});
       }
-    }, delay * position)
+    }, promiseCalingDelay)
   });
 }
